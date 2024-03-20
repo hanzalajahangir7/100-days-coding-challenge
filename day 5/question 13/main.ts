b05df9fc2a7378c2dbb04f5de90af4965f63a496
hanzalajahangir7 <lyneskroff2189@gmail.com>
2024-03-20 07:04:02 +0500

    day 5

---
+++ day 5/question 13/main.ts
@@ -0,0 +1,7 @@
+// Question 13: Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+let guests: string[] = ["Albert Einstein", "Marie Curie", "Nikola Tesla"];
+
+guests.forEach(guest => {
+    console.log(`Dear ${guest}, would you like to join me for dinner?`);
+});
