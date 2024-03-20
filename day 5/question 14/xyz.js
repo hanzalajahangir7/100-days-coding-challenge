84f42a69bfb1c7e51a7358c164863dd8594aaaeb
hanzalajahangir7 <lyneskroff2189@gmail.com>
2024-03-20 06:56:02 +0500

    day 5 qs 14

---
+++ day 5/question 14/xyz.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Step 1: Initialize the Guest List Array
+let guestList = ["Alice", "Bob", "Charlie", "David", "Eve"];
+// Step 2: Replacing a Guest
+const guestWhoCantMakeIt = "Charlie";
+const replacementGuest = "Frank";
+const index = guestList.indexOf(guestWhoCantMakeIt);
+if (index !== -1) {
+    // Replace the guest who can't make it with the replacement guest
+    guestList[index] = replacementGuest;
+}
+else {
+    console.log(`${guestWhoCantMakeIt} is not in the guest list.`);
+}
+// Step 3: Sending New Invitations
+console.log("Sending out new invitations:");
+guestList.forEach(guest => {
+    console.log(`Dear ${guest}, you are invited to the dinner.`);
+});
