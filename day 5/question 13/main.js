8560af94d14ed3337805dd08d23baeac1ede6762
hanzalajahangir7 <118133960+hanzalajahangir7@users.noreply.github.com>
2024-03-20 06:57:15 +0500

    Delete day 5/question 13 directory

--- day 5/question 13/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let guests = ["Albert Einstein", "Marie Curie", "Nikola Tesla"];
-guests.forEach(guest => {
-    console.log(`Dear ${guest}, would you like to join me for dinner?`);
-});
