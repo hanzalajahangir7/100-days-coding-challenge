bae6ec76ab3a7442bfc3f1f8ae7bd66175b98c21
hanzalajahangir7 <lyneskroff2189@gmail.com>
2024-03-20 06:52:35 +0500

    new commit

---
+++ day 5/question 13/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let guests = ["Albert Einstein", "Marie Curie", "Nikola Tesla"];
+guests.forEach(guest => {
+    console.log(`Dear ${guest}, would you like to join me for dinner?`);
+});
